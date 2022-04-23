63954f7c77f0c3f33f8d586be711f7b925e67c21
Leonardo Carvalho Vilela <leovilela100@gmail.com>
2022-04-23 18:57:19 -0300

    Autoload Hcodebr class

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit165789d27d862a695c67d5378f7f88f0
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit165789d27d862a695c67d5378f7f88f0
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit165789d27d862a695c67d5378f7f88f0::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit165789d27d862a695c67d5378f7f88f0::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit165789d27d862a695c67d5378f7f88f0::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit165789d27d862a695c67d5378f7f88f0::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit165789d27d862a695c67d5378f7f88f0::$classMap;
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'e2b3962751d404f383b84b8fa3e7e9934b46bf52',
         'name' => 'hcodebr/ecommerce',
         'dev' => true,
     ),
     'versions' => array(
         'hcodebr/ecommerce' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'e2b3962751d404f383b84b8fa3e7e9934b46bf52',
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
