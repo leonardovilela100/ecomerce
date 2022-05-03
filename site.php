e111d0cf1c50b6cc4097ad7d95233896778bd0d2
Leonardo Carvalho Vilela <leovilela100@gmail.com>
2022-05-02 23:49:14 -0300

    Criando a Paginação

--- site.php
+++ site.php
@@ -4,8 +4,6 @@ use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
 
-
-
 $app->get('/', function() {
 
 	$products = Product::listAll();
@@ -18,21 +16,34 @@ $app->get('/', function() {
 
 });
 
-$app->get("/categories/:idcategory", function($idcategory){
+$app->get("/categories/:idcategory", function($idcategory) {
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+	
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+	
+	$pagination = $category->getProductsPage($page);
+	
+	$pages = [];
+	
+	for ($i=1; $i <= $pagination['pages']; $i++) {
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
 
 	$page = new Page();
 
 	$page->setTpl("category", [
 		'category'=>$category->getValues(),
-		'products'=>Product::checklist($category->getProducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	]);
 
 });
-
 
 
 
